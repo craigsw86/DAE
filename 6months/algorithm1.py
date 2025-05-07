628ef2b75e5dde1b0161c87cd4adf4a44dc5bf5a
craigsw86 <craig.s.weinstein@gmail.com>
2025-05-07 12:03:29 -0400

    update

--- 6months/algorithm1.py
+++ 6months/algorithm1.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 def is_prime(n):
     if n <= 1:
         return False # 0 and 1 are not prime numbers
@@ -8,7 +10,7 @@ def is_prime(n):
     
     # Only check odd divisors up to the square root of n
     for i in range (3, int(n**0.5) + 1, 2):
-        if n % i == 0
+        if n % i == 0:
             return False
     
     return True
