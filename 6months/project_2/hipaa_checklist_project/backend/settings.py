86c5781d766f189f93d748b63e966660c7d58fa7
craigsw86 <craig.s.weinstein@gmail.com>
2025-07-18 16:49:00 -0400

    Doing catch-up work

---
+++ 6months/project_2/hipaa_checklist_project/backend/settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hipaa_checklist',
+        'USER': 'youruser',
+        'PASSWORD': 'yourpass',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
