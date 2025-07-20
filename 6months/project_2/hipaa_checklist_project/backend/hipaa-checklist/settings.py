5c1586f2a1390fecce1be058246cd76741dbcbd3
craigsw86 <craig.s.weinstein@gmail.com>
2025-07-20 17:41:59 -0400

    Weekend Work Update 2

--- 6months/project_2/hipaa_checklist_project/backend/hipaa-checklist/settings.py
+++ 6months/project_2/hipaa_checklist_project/backend/hipaa-checklist/settings.py
@@ -9,3 +9,8 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS = [..., 'auditlog', 'encrypted_models_fields', 'rest_framework_simplejwt']
+REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)}
+FIELD_ENCRYPTION_KEYS = ['secure-key-here'] # Generate via fernet
+DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'hipaa_db', ...}}
+AUDITLOG_INCLUDE_ALL_MODELS = True
